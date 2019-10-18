2236a32f4a4412ecd4ba6038bcfbb71c50e10dae
pdxdave <webdevdave@hotmail.com>
2019-10-18 09:57:29 -0700

    a snag. had to make changes

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,8 +1,23 @@
 """CPU functionality."""
 # Round Two on this assignment.  Make it easier
+# Ughh...I want to cry.  Moving the Run CPU section out
+# of the class CPU and creating a recursive call whacked my
+# code. Had to redue all my elif statements to def
+
 
 import sys
-import re
+
+# Pulled this out of the class and brought it to the top
+"""Run the CPU."""
+HLT  = 0b00000001  
+LDI  = 0b10000010  
+PRN  = 0b01000111  
+PUSH = 0b01000101  
+POP  = 0b01000110  
+MUL  = 0b10100010
+CALL = 0b01010000
+RET  = 0b00010001  
+ADD  = 0b10100000
 
 class CPU:
     """Main CPU class."""
@@ -14,6 +29,17 @@ class CPU:
         self.pc = 0
         self.reg[7] = 255
         self.SP = 0x07
+        # This is where the CPU actions will be run from
+        self.cpuruntable = {
+            LDI: self.LDI,
+            PRN: self.PRN,
+            PUSH: self.PUSH,
+            POP: self.POP,
+            MUL: self.MUL,
+            ADD: self.ADD,
+            CALL: self.CALL,
+            RET: self.RET
+        }
 
     def ram_read(self, MAR):
         """ should accept the address to read and return the value stored there."""
@@ -83,62 +109,51 @@ class CPU:
 
         print()
 
+    
+    def LDI(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+        self.pc += 3
+    def PRN(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+        self.pc += 2
+    def MUL(self, operand_a, operand_b):
+        self.alu("MUL", operand_a, operand_b)
+        self.pc += 3
+    def ADD(self, operand_a, operand_b):
+        self.alu("ADD", operand_a, operand_b)
+        self.pc += 3
+    def PUSH(self, operand_a, operand_b):
+        self.SP -= 1
+        self.ram[self.SP] = self.reg[operand_a]
+        self.pc += 2
+    def POP(self, operand_a, operand_b):
+        self.reg[operand_a] = self.ram[self.SP]
+        self.SP += 1
+        self.pc += 2
+    def CALL(self, operand_a, operand_b):
+        ret_add = self.pc + 2
+        self.reg[self.SP] -= 1
+        self.ram[self.reg[self.SP]] = ret_add
+        self.pc = self.reg[operand_a]
+    def RET(self, operand_a, operand_b):
+        ret_add = self.ram[self.reg[self.SP]]
+        self.reg[self.SP] += 1
+        self.pc = ret_add
     def run(self):
-        """Run the CPU."""
-        HLT  = 0b00000001  
-        LDI  = 0b10000010  
-        PRN  = 0b01000111  
-        PUSH = 0b01000101  
-        POP  = 0b01000110  
-        MUL  = 0b10100010
-        CALL = 0b01010000
-        RET  = 0b00010001  
-        ADD  = 0b10100000
-
-        running = True
-        while running:
-            # this comes right out of the instructions. It talks about reading the memory
-            # address stored in the register 'pc' and storing it in the 'ir'
-            IR = self.ram[self.pc]
-            # again, from the instructions it's saying to using the ram_read() with pc+1, pc+2 and
-            # operand a and b
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
-            # if the IR is Halted, this isn't running 
-            if IR == HLT:
-                running = False
-            elif IR == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            # elif print  equals IR
-            elif IR == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
-            elif IR == ADD:
-                self.alu("ADD", operand_a, operand_b)
-                self.pc += 3
-            elif IR == MUL:
-                self.alu("MUL", operand_a, operand_b)
-                self.pc += 3
-            elif IR == PUSH:
-                self.SP -= 1
-                self.ram[self.SP] = self.reg[operand_a]
-                self.pc += 2
-            elif IR == POP:
-                self.reg[operand_a] = self.ram[self.SP]
-                self.SP += 1
-                self.pc += 2
-            elif IR == CALL: 
-                return_address = self.pc + 2
-                self.reg[self.SP] -= 1
-                self.ram[self.reg[self.SP]] = return_address
-                self.pc = self.reg[operand_a]
-            elif IR == RET: 
-                return_address = self.ram[self.reg[self.SP]]
-                self.reg[self.SP] += 1
-                self.pc = return_address
-            else:
-                print("Halting the program")
-                running = False
-            
+        
+            """ New location of Run the CPU """
+            running = True
+            while running:
+                # this comes right out of the instructions. It talks about reading the memory
+                # address stored in the register 'pc' and storing it in the 'ir'
+                IR = self.ram[self.pc]
+                # again, from the instructions it's saying to using the ram_read() with pc+1, pc+2 and
+                # operand a and b
+                operand_a = self.ram_read(self.pc + 1)
+                operand_b = self.ram_read(self.pc + 2)
+                # if the IR is Halted, this isn't running 
+                if IR == HLT:
+                    running = False
+                else:
+                    self.cpuruntable[IR](operand_a, operand_b)
 
